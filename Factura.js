7478884f6d005738e96f00741742d1c621d9dd47
SebastianCasanova4 <s.casanovao@uniandes.edu.co>
2024-07-29 19:08:30 -0500

    nueva prueba script imagen

--- Factura.js
+++ Factura.js
@@ -118,10 +118,9 @@ function insertarImagen(fila) {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Historial Facturas');
   var imageUrl = 'https://cdn.icon-icons.com/icons2/1674/PNG/512/download_111133.png'; // Reemplaza con la URL de tu imagen
   var cell = sheet.getRange('F'+fila);
-  var image = SpreadsheetApp.newCellImage().setSourceUrl(imageUrl).build();
-  cell.setValue(image);
-  //generarPDFfactura
-  //var image = sheet.insertImage(imageUrl, cell.getColumn(), cell.getRow(), 1, 1);
+  var imageBlob = UrlFetchApp.fetch(imageUrl).getBlob();
+  var image = sheet.insertImage(imageBlob, cell.getColumn(), cell.getRow());
+  image.assignScript("resetPlantilla");
 }
 
 function generarPDFfactura(numeroFactura) {
