b7a441c529740c08ecb3f157059853084a4ac3cd
Catanoca0202 <catano.a.c2002@gmail.com>
2024-08-09 10:09:44 -0500

    mas cambios

--- productos.js
+++ productos.js
@@ -10,7 +10,7 @@ function obtenerInformacionProducto(producto) {
   
     let codigoProducto = datos_sheet.getRange("H11").getValue();
     let valorUnitario = datos_sheet.getRange("J11").getValue();
-    let porcientoIva = String(datos_sheet.getRange("K11").getValue());
+    let porcientoIva = datos_sheet.getRange("K11").getValue();
     let precioConIva = datos_sheet.getRange("L11").getValue();
     let impuestos = datos_sheet.getRange("M11").getValue();
     let descunetos=datos_sheet.getRange("N11").getValue();
