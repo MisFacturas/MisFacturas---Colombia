d8d57af3fdceca06b8e8ef20489f18f17a1a2c5e
Catanoca0202 <catano.a.c2002@gmail.com>
2024-08-12 23:45:19 -0500

    otro pequeno ajuste

--- Factura.js
+++ Factura.js
@@ -624,10 +624,10 @@ function limpiarHojaFactura(){
   hojaFactura.getRange("B15").setValue("")//producto
   hojaFactura.getRange("C15").setValue("")//cantidad
   hojaFactura.getRange("A15").setValue("")//referncia
-  hojaFactura.getRange("G15").setValue("0")
-  hojaFactura.getRange("G15").setNumberFormat("0,00%")//IVA%
+  hojaFactura.getRange("H15").setValue("0")
+  hojaFactura.getRange("G15").setValue("")//IVA%
   hojaFactura.getRange("D15").setValue("")//sinIva
-  hojaFactura.getRange("H15").setValue("")//descuento
+  hojaFactura.getRange("H15").setNumberFormat("0%")//descuento
   hojaFactura.getRange("I15").setValue("")//retencion
   hojaFactura.getRange("J15").setValue("")//recargo
 
